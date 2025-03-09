ae20b1a7af7d29665342120d54a891d8f8e9ab33
Elias Peschke <eliaspeschke@googlemail.com>
2025-03-09 22:17:24 +0100

    wip

--- home/nixvim/default.nix
+++ home/nixvim/default.nix
@@ -32,6 +32,7 @@ in
 	sources = [
 	  { 
 	     name = "nvim_lsp";
+             priority = 10;
 	  }
 	  {
 	    name = "path";
@@ -43,8 +44,7 @@ in
 	  "C-p" = "cmp.mapping.select_prev_item()";
 	  "C-b" = "cmp.mapping.scroll_docs(-4)";
 	  "C-f" = "cmp.mapping.scroll_docs(4)";
-	  "<CR>" = "cmp.mapping.confirm { select = true }";
-	  "C-y" = "cmp.mapping.complete";
+	  "C-y" = "cmp.mapping.confirm { select = true }";
 	};
       };
     };
