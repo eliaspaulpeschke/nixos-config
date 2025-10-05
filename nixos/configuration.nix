723c97076d009f0123ecf4c0175209c1b4741008
Elias Peschke <eliaspeschke@googlemail.com>
2025-10-05 14:57:53 +0200

    add evemu

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -83,6 +83,7 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    evemu
     wget
     git
     xdg-user-dirs
