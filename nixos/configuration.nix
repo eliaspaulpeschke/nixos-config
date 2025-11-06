4f2c902c2df63a15eddcaf6883b062e583e269e4
Elias Peschke <eliaspeschke@googlemail.com>
2025-11-06 13:08:46 +0100

    add pamixer, pavucontrol

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -91,6 +91,8 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    pamixer
+    pavucontrol
     wev
     socat
     wget
