7e06544a0dbe6997362eb50f2dbd4babf555f19f
Elias Peschke <eliaspeschke@googlemail.com>
2025-12-17 21:45:59 +0100

    new flake, no ghc 98

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -95,8 +95,8 @@
     xdg-user-dirs
     man-pages
     man-pages-posix
-    (haskell-language-server.override { 
-             supportedGhcVersions = [ "96" "98"];
+    (haskell-language-server .override { 
+             supportedGhcVersions = [ "96" "910"];
     })
     brightnessctl
   ];
